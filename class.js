a4be352955d953bb5b898800f1076a4a87c0aed5
vamkrtch <vam.golden.group.j@gmail.com>
2022-06-04 21:36:39 +0400

    Animal extends GrassEater and took chooseCell

--- class.js
+++ class.js
@@ -158,8 +158,9 @@ class GrassEater extends Grass {
         }
     }
 }
-class Animal {
+class Animal extends GrassEater {
     constructor(x, y, index) {
+        super(x, y, index);
         this.x = x;
         this.y = y;
         this.energy = 8;
@@ -194,23 +195,11 @@ class Animal {
 
         ];
     }
+
     chooseCell(character) {
         this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-
-        return found;
+        return super.chooseCell(character);
     }
-
-
     move() {
 
 
