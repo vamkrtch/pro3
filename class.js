6409607fec232be453ef477322b5f0714ac8b352
vamkrtch <vam.golden.group.j@gmail.com>
2022-06-04 21:28:00 +0400

    popoxel em u grass eater@ jarangel e grass -ic chooseCell funtion

--- class.js
+++ class.js
@@ -43,14 +43,15 @@ class Grass {
         }
     }
 
-
-
+    
+    
 }
 
 
 
-class GrassEater {
+class GrassEater extends Grass {
     constructor(x, y, index) {
+        super(x, y, index);
         this.x = x;
         this.y = y;
         this.energy = 8;
@@ -70,24 +71,13 @@ class GrassEater {
     }
     chooseCell(character) {
         this.getNewCoordinates();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-
-        return found;
+        return super.chooseCell(character);
     }
-
-
+    
+    
     move() {
-
-
+        
+        
         var newCell = random(this.chooseCell(0))
 
         if (newCell) {
